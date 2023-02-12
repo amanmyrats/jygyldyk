1d697d3fe6a869ec5ba214d6045592870aca89e6
amanmyrats <amansarahs@gmail.com>
2023-02-12 15:15:53 +0500

    Dockerizing

--- ecommerce/core/settings.py
+++ ecommerce/core/settings.py
@@ -182,10 +182,10 @@ DATABASES = {
     "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
         "NAME": os.environ.get("SQL_DB", str(BASE_DIR / "db.sqlite3")),
-        "USER": os.environ.get("SQL_USER", "aman"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "aman"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+        "USER": os.environ.get("SQL_USER", ""),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", ""),
+        "HOST": os.environ.get("SQL_HOST", ""),
+        "PORT": os.environ.get("SQL_PORT", ""),
     }
 }
 
